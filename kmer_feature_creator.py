2b4f873277ec01299e1fac57538f2af5ce13643a
Aditya Sharma <5310731+kbrunnerLXG@users.noreply.github.com>
2026-01-11 11:59:34 +0800

    Done a simple ExtraTrees and RandomForest Classification model, does need to do one with a sequence similarity clusters maybe using CD-HIT

--- kmer_feature_creator.py
+++ kmer_feature_creator.py
@@ -9,46 +9,53 @@ kmer_vocab = set()
 no_of_proteins = 0
 proteins = []
 y=[]
+protein_id = []
+data = {}
 
-for record in SeqIO.parse('/home/aditya/hmmvml/vpR_vpx/protein-matching-IPR000012.fasta', "fasta"):
+for record in SeqIO.parse('/home/aditya/hmmvml/unreviewed/protein-matching-IPR000012.fasta', "fasta"):
     if "Vpr"  in str(record.description):
         y.append(0)
     elif "Vpx" in str(record.description):
         y.append(1)
     else:
         continue
+    protein_id.append(str(record.id).split("|")[0])
+    proteins.append(str(record.seq))
 
-    no_of_proteins += 1
-    proteins.append(record.seq)
-    seq = record.seq
-    for i in range(len(seq) - kmer_length + 1):
-        subseq = seq[i:i + kmer_length]
-        if subseq not in kmer_vocab:
-            kmer_vocab.add(subseq)
-
-
-num_kmers = len(kmer_vocab)
-
-kmer_index = {kmer: j for j, kmer in enumerate(kmer_vocab)}
-
-
-table = [[0] * num_kmers for _ in range(no_of_proteins)]
-for i, seq in enumerate(proteins):
-    for pos in range(len(seq) - kmer_length + 1):
-        kmer = seq[pos:pos + kmer_length]
-        j = kmer_index[kmer]
-        table[i][j] = 1
-
-table = np.array(table)
-print(len(proteins))
-print(len(kmer_vocab))
-print((table.shape))
-print(pd.Series(y).value_counts())
-
-
-with open('data.pkl', 'wb',) as f:
-    pkl.dump(table,f)
-
-with open('label.pkl', 'wb') as f:
-    pkl.dump(y,f)
+df = pd.DataFrame({"Protein_id": protein_id,"Sequence":proteins,"Label":y})
+print(df.head())
+#
+#     no_of_proteins += 1
+#     proteins.append(record.seq)
+#     # seq = record.seq
+# #     for i in range(len(seq) - kmer_length + 1):
+# #         subseq = seq[i:i + kmer_length]
+# #         if subseq not in kmer_vocab:
+# #             kmer_vocab.add(subseq)
+# #
+# #
+# # num_kmers = len(kmer_vocab)
+# #
+# # kmer_index = {kmer: j for j, kmer in enumerate(kmer_vocab)}
+#
+#
+# table = [[0] * num_kmers for _ in range(no_of_proteins)]
+# for i, seq in enumerate(proteins):
+#     for pos in range(len(seq) - kmer_length + 1):
+#         kmer = seq[pos:pos + kmer_length]
+#         j = kmer_index[kmer]
+#         table[i][j] = 1
+#
+# table = np.array(table)
+# print(len(proteins))
+# print(len(kmer_vocab))
+# print((table.shape))
+# print(pd.Series(y).value_counts())
+#
+#
+# with open('data.pkl', 'wb',) as f:
+#     pkl.dump(table,f)
+#
+# with open('label.pkl', 'wb') as f:
+#     pkl.dump(y,f)
 
